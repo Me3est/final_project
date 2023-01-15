7e9ec3ecbd65522817b24a5aa59db4a06414fcfb
me3est <me3est@gmail.com>
2023-01-15 19:27:27 +0900

    final

--- stl/js/login.js
+++ stl/js/login.js
@@ -1,4 +1,4 @@
-let check_box = document.getElementsByClassName('.check_box')[0];
+let check_box = document.getElementsByClassName('check_box')[0];
 check_box.addEventListener('click', function() {
-    
+    check_box.background="background: url(../img/icon/20210728014159627.png) no-repeat center / cover;"
 })
