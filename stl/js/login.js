1e609b469f840587397646fab225c5635e6e4a1d
me3est <me3est@gmail.com>
2023-01-16 16:54:02 +0900

    final

--- stl/js/login.js
+++ stl/js/login.js
@@ -1,4 +1,34 @@
-let check_box = document.getElementsByClassName('check_box')[0];
-check_box.addEventListener('click', function() {
-    check_box.background="background: url(../img/icon/20210728014159627.png) no-repeat center / cover;"
+// let check_box = document.getElementsByClassName('check_box')[0];
+// check_box.addEventListener('click', function() {
+//     check_box.background="background: url(../img/icon/20210728014159627.png) no-repeat center / cover;"
+// })
+$(document).ready(function() {
+    $('#member_login_tab').click(function() {
+        $('.member_login_pan').css({display:'block'});
+        $('.not_member_login_pan').css({display:'none'});
+        $('#not_member_login').css({
+            color: '#888',
+            border: '1px solid #efefef',
+            backgroundColor: '#fafafa',
+        })
+        $('#member_login_tab').css({
+            color:'#222',
+            border: '1px solid #aaa',
+            backgroundColor: '#fff'
+        })
+    })
+    $('#not_member_login').click(function() {
+        $('.member_login_pan').css({display:'none'});
+        $('.not_member_login_pan').css({display:'block'});
+        $('#not_member_login').css({
+            color:'#222',
+            border: '1px solid #aaa',
+            backgroundColor: '#fff'
+        })
+        $('#member_login_tab').css({
+            color: '#888',
+            border: '1px solid #efefef',
+            backgroundColor: '#fafafa',
+        })
+    })
 })
